73e98c55060d3bb69d17be914039239a755535f9
chsafouane <chsafouane@gmail.com>
2019-07-09 10:01:50 +0200

    Added doc for fbind

--- R/fbind.R
+++ R/fbind.R
@@ -1,3 +1,15 @@
+#' Combine two factors
+#'
+#' Coerces each input factor to character, combines them, then converts
+#' back to a factor. New levels are the union of the levels of the inputs.
+#'
+#' @param a factor
+#' @param b factor
+#'
+#' @return factor
+#' @export
+#'
+#' @examples
 fbind <- function(a, b){
   factor(c(as.character(a), as.character(b)))
 }
