166ed3ba3005a1d98c66dc019159ebf760b12d01
chsafouane <chsafouane@gmail.com>
2019-07-09 10:03:38 +0200

    Modified doc of fbind

--- R/fbind.R
+++ R/fbind.R
@@ -3,10 +3,10 @@
 #' Coerces each input factor to character, combines them, then converts
 #' back to a factor. New levels are the union of the levels of the inputs.
 #'
-#' @param a factor
-#' @param b factor
+#' @param a A factor
+#' @param b A factor
 #'
-#' @return factor
+#' @return A factor
 #' @export
 #'
 #' @examples
